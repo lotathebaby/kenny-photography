6dd2352f639b9800ce6cf6284ce3fc81f01b1205
Kosisochukwu Obidiegwu <lotanna@Kosisochukwus-Laptop.local>
2025-08-31 23:47:53 -0500

    update

--- index.js
+++ index.js
@@ -23,3 +23,22 @@ setInterval(() => {
   currentImage = (currentImage + 1) % backgroundImages.length;
   changeBackground();
 }, 5000);
+
+const menuBtn = document.getElementById('menuBtn');
+const closeBtn = document.getElementById('closeBtn');
+const mobileMenu = document.getElementById('mobileMenu');
+const backdrop = document.getElementById('backdrop');
+
+const openMenu = () => {
+  mobileMenu.classList.remove('translate-x-full');
+  backdrop.classList.remove('hidden');
+};
+
+const closeMenu = () => {
+  mobileMenu.classList.add('translate-x-full');
+  backdrop.classList.add('hidden');
+};
+
+menuBtn.addEventListener('click', openMenu);
+closeBtn.addEventListener('click', closeMenu);
+backdrop.addEventListener('click', closeMenu);
